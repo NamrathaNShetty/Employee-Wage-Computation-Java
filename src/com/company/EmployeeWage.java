f3898bea220fb20d9a9a798e9cc2345aafb36fea
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-03 14:17:36 +0530

    Added: Employee Attendance

--- src/com/company/EmployeeWage.java
+++ src/com/company/EmployeeWage.java
@@ -3,6 +3,16 @@ package com.company;
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+
+        int IS_PRESENT = 1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println(" Employee is Present");
+        else
+            System.out.println(" Employee is Absent");
     }
 }
+
+
+
