3fcc40cb2b5827666bd3e610a667215a80cfbd7c
NamrathaNShetty <namratanshetty@gmail.com>
2021-08-16 17:21:03 +0530

    Added: Manage employee wage for multiple companies using interface

--- src/com/company/EmployeeWage.java
+++ src/com/company/EmployeeWage.java
@@ -1,23 +1,26 @@
 package com.company;
 
-public class EmployeeWage {
+public class EmployeeWage implements IComputeWage{
     public static final int isPartTime=0;
     public static final int isFullTime=1;
     private int numOfCompany;
     private CompanyEmpWage empWageArray[];
-
     public EmployeeWage() {
         empWageArray=new CompanyEmpWage[5];
     }
-    private void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth) {
+    @Override
+    public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
         empWageArray[numOfCompany]=new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
         numOfCompany++;
     }
-    private void computeEmpWage() {
+
+    @Override
+    public void computeEmpWage() {
         for(int i=0;i<numOfCompany;i++) {
             empWageArray[i].setTotalEmpWage(this.computeEmpWage(empWageArray[i]));
             System.out.println(empWageArray[i]);
         }
+
     }
     private int computeEmpWage(CompanyEmpWage companyEmpWage) {
         int empHours=0, totalEmpHours=0, totalWorkingDays=0;
@@ -46,4 +49,6 @@ public class EmployeeWage {
         employeeWage.computeEmpWage();
 
     }
+
+
 }
